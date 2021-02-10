b0247d9a25815435d1067d40e67fcd29fde38d13
Alex Martin <atonymartin20@yahoo.com>
2021-02-09 23:36:18 -0600

    Created About Component

---
+++ src/about/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class About extends React.Component {
+    state={
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default About;
