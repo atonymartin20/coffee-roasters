e89ff9440486a494c39546800e3fe7c9a533265c
Alex Martin <atonymartin20@yahoo.com>
2021-02-09 23:36:26 -0600

    Created Homepage Component

---
+++ src/homepage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+    state={
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Homepage;
