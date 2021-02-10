620b4ae168588f0bca0e2376cd00494a5658be31
Alex Martin <atonymartin20@yahoo.com>
2021-02-09 23:36:39 -0600

    Created Subscribe Component

---
+++ src/subscribe/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Subscribe extends React.Component {
+    state={
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Subscribe;
