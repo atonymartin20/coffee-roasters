aae3fb3937f78f6df43201a2a9d78b67ec907feb
Alex Martin <atonymartin20@yahoo.com>
2021-02-10 23:56:42 -0600

    Added some styling notes to Homepage

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navbar from '../navbar';
+import Footer from '../footer';
 
 class Homepage extends React.Component {
     state={
@@ -8,7 +10,31 @@ class Homepage extends React.Component {
     render() {
         return(
             <div>
+				<Navbar />
+				<header>
+					<div>
+						{/* Header Image 
+							1280px X 600px */}
+						{/* 135px bottom margin */}
 
+						{/* Coffee Section
+							1111px X 525px */}
+						{/* 200px bottom margin */}
+
+						{/* Section
+							1280px X 728px */}
+						{/* 200px bottom margin */}
+
+						{/* Section
+							1045px X 565px */}
+						{/* 60px bottom margin */}
+
+						{/* Button left aligned
+							217px X 56px */}
+						{/* 200px bottom margin */}
+					</div>
+				</header>
+				<Footer />
             </div>
         )
     }
