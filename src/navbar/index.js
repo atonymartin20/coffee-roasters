a4791d3348c9811f99f83be8e2715c19e579bc4c
Alex Martin <atonymartin20@yahoo.com>
2021-02-19 21:41:21 -0600

    Update index.js

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../assets/shared/desktop/logo.svg';
 import { NavLink } from 'react-router-dom';
 import HamburgerIcon from '../assets/shared/mobile/icon-hamburger.svg';
-import CloseIcon from '../assets/shared/mobile/icon-close.svg';
+// import CloseIcon from '../assets/shared/mobile/icon-close.svg';
 import Dropdown from './dropdown.js';
 
 class Navbar extends React.Component {
@@ -25,25 +25,6 @@ class Navbar extends React.Component {
     }
 
     render() {
-        // Styling Notes
-        // Desktop
-            // Total Navbar width 100% height 33px padding: 0
-            // margin top and bottom 40px
-            // flex align-Items: center
-            // justify-content: space-between
-
-        // Tablet
-            // Total Navbar 689px X 26px
-            // margin-top 40px
-            // margin-bottom 52px
-
-        // Mobile
-            // Total Navbar 327px X 18px
-            // Hamburger 16px X 15px
-
-        // Logo 235 X 25
-        // Nav Section 283 X 15
-
         return(
             <>
                 {this.state.open ? 
