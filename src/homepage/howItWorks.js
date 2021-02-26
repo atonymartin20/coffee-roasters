cd590e4cbbd18a01983f1e492deda97eb2d7bc4f
Alex Martin <atonymartin20@yahoo.com>
2021-02-25 23:17:23 -0600

    Update howItWorks.js

--- src/homepage/howItWorks.js
+++ src/homepage/howItWorks.js
@@ -18,23 +18,23 @@ class HowItWorks extends React.Component {
                         <div class='how-it-works-box-div'>
                             <h1>01</h1>
                             <h3>Pick your coffee</h3>
-                            Select from our evolving range of artisan coffees. Our beans are ethically 
+                            <body>Select from our evolving range of artisan coffees. Our beans are ethically 
                             sourced and we pay fair prices for them. There are new coffees in all profiles 
-                            every month for you to try out. 
+                            every month for you to try out.</body> 
                         </div>
 
                         <div class='how-it-works-box-div'>
                             <h1>02</h1>
                             <h3>Choose the frequency</h3>
-                            Customize your order frequency, quantity, even your roast style and grind type. 
-                            Pause, skip or cancel your subscription with no commitment through our online portal.
+                            <body>Customize your order frequency, quantity, even your roast style and grind type. 
+                            Pause, skip or cancel your subscription with no commitment through our online portal.</body>
                         </div>
 
                         <div class='how-it-works-box-div'>
                             <h1>03</h1>
                             <h3>Receive and enjoy!</h3>
-                            We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning 
-                            world-class coffees curated to provide a distinct tasting experience.
+                            <body>We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning 
+                            world-class coffees curated to provide a distinct tasting experience.</body>
                         </div>
 
                     </div>
