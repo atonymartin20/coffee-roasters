76e3a7a01ed1ce99ca5ee8ff76af7babbf18e1f4
Alex Martin <atonymartin20@yahoo.com>
2021-03-02 23:41:55 -0600

    Created and completed Header

---
+++ src/subscribe/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header class='subscribe-header'>
+                <div class='subscribe-header-text-container'>
+                    <h2>Create a plan</h2>
+
+                    <body>
+                        Build a subscription plan that best fits your needs. We offer an assortment of the best 
+                        artisan coffees from around the globe delivered fresh to your door.
+                    </body>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
