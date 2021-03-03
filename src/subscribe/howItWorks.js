89f6b19b9910b5470a4b2e3e43fa7201da76c4f1
Alex Martin <atonymartin20@yahoo.com>
2021-03-02 23:42:30 -0600

    Created and completed How It Works completed

---
+++ src/subscribe/howItWorks.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class HowItWorks extends React.Component {
+    render() {
+        return(
+            <div class='subscribe-how-it-works-div'>
+                <h4>How it works</h4>
+
+                <div class='subscribe-how-it-works-large-container-div'>
+                    <div class='subscribe-horizontal-line-div'>
+                        <div class='subscribe-circle-div' />
+                        <div class='subscribe-circle-div' />
+                        <div class='subscribe-circle-div' />
+                    </div>
+
+                    <div class='subscribe-how-it-works-box-container-div'>
+                        <div class='subscribe-how-it-works-box-div'>
+                            <h1>01</h1>
+                            <h3>Pick your coffee</h3>
+                            <body>Select from our evolving range of artisan coffees. Our beans are ethically 
+                            sourced and we pay fair prices for them. There are new coffees in all profiles 
+                            every month for you to try out.</body> 
+                        </div>
+
+                        <div class='subscribe-how-it-works-box-div'>
+                            <h1>02</h1>
+                            <h3>Choose the frequency</h3>
+                            <body>Customize your order frequency, quantity, even your roast style and grind type. 
+                            Pause, skip or cancel your subscription with no commitment through our online portal.</body>
+                        </div>
+
+                        <div class='subscribe-how-it-works-box-div'>
+                            <h1>03</h1>
+                            <h3>Receive and enjoy!</h3>
+                            <body>We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning 
+                            world-class coffees curated to provide a distinct tasting experience.</body>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default HowItWorks;
