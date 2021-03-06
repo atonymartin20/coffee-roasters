8751c4eb8700bb36406f9df067207b0dacf36989
Alex Martin <atonymartin20@yahoo.com>
2021-03-05 22:53:42 -0600

    Starting to work on variables

--- src/subscribe/index.js
+++ src/subscribe/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import Navbar from '../navbar';
+import Header from './header.js';
+import HowItWorks from './howItWorks.js';
+import ChoicesContainer from './choicesContainer.js';
 import Footer from '../footer';
 import '../css/subscribe.css';
 
 class Subscribe extends React.Component {
     state={
+        preferences: '_____',
+        bean: '_____',
+        quantity: '_____',
+        grind: '_____',
+        deliveries: '_____',
 
+        // handleChange = (event, varName) => {
+
+        // }
     }
 
     render() {
         return(
-            <div>
+            <div class='subscribe-outside-div'>
 				<Navbar />
+                <Header />
+                <HowItWorks />
+                <ChoicesContainer preferences={this.state.preferences} bean={this.state.bean} quantity={this.state.quantity} grind={this.state.grind} deliveries={this.state.deliveries} />
 				<Footer />
             </div>
         )
