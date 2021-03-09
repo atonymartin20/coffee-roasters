bf34877824fe3c7abe197161b09937f9dbb923b1
Alex Martin <atonymartin20@yahoo.com>
2021-03-09 12:36:09 -0600

    CreatePlanButton fixed for when capsule preference is chosen

--- src/subscribe/createPlanButton.js
+++ src/subscribe/createPlanButton.js
@@ -2,17 +2,33 @@ import React from 'react';
 
 class CreatePlanButton extends React.Component {
 	render() {
-		return (
-			<div className="subscribe-create-your-plan-button-div">
-				<button
-					className="subscribe-create-your-plan-button"
-					onClick={this.props.openModal}
-                    disabled={this.props.preferences === '_____' || this.props.bean === '_____' || this.props.quantity === '_____' || this.props.grind === '_____' || this.props.deliveries === '_____'}
-				>
-					Create your plan
-				</button>
-			</div>
-		);
+        if (this.props.preferences === 'Capsules') {
+            return (
+                <div className="subscribe-create-your-plan-button-div">
+                    <button
+                        className="subscribe-create-your-plan-button"
+                        onClick={this.props.openModal}
+                        disabled={this.props.preferences === '_____' || this.props.bean === '_____' || this.props.quantity === '_____' || this.props.deliveries === '_____'}
+                    >
+                        Create your plan
+                    </button>
+                </div>
+            );
+        }
+
+        else {
+            return (
+                <div className="subscribe-create-your-plan-button-div">
+                    <button
+                        className="subscribe-create-your-plan-button"
+                        onClick={this.props.openModal}
+                        disabled={this.props.preferences === '_____' || this.props.bean === '_____' || this.props.quantity === '_____' || this.props.grind === '_____' || this.props.deliveries === '_____'}
+                    >
+                        Create your plan
+                    </button>
+                </div>
+            );
+        }
 	}
 }
 
