60a8250163396ac4b193660fb9be5425119da297
Alex Martin <atonymartin20@yahoo.com>
2021-03-08 23:51:39 -0600

    Passed props in choicesContainer

--- src/subscribe/choicesContainer.js
+++ src/subscribe/choicesContainer.js
@@ -5,19 +5,23 @@ import BeanType from './choiceContainers/beanType.js';
 import Quantity from './choiceContainers/quantity.js';
 import GrindOption from './choiceContainers/grindOption.js';
 import Deliveries from './choiceContainers/deliveries.js';
+import OrderSummary from './orderSummary.js';
+import CreatePlanButton from './createPlanButton.js';
 
 class ChoicesContainer extends React.Component {
     render() {
         return(
-            <div class='subscribe-choices-container-div'>
+            <div className='subscribe-choices-container-div'>
                 <DesktopTracker preferences={this.props.preferences} bean={this.props.bean} quantity={this.props.quantity} grind={this.props.grind} deliveries={this.props.deliveries}/>
 
-                <div class='subscribe-choices-right-container-div'>
-                    <Preferences />
-                    <BeanType />
-                    <Quantity />
-                    <GrindOption />
-                    <Deliveries />
+                <div className='subscribe-choices-right-container-div'>
+                    <Preferences preferences={this.props.preferences} updatePreferences={this.props.updatePreferences} />
+                    <BeanType preferences={this.props.preferences} bean={this.props.bean} updateBean={this.props.updateBean} />
+                    <Quantity bean={this.props.bean} quantity={this.props.quantity} updateQuantity={this.props.updateQuantity} />
+                    <GrindOption quantity={this.props.quantity} grind={this.props.grind} updateGrind={this.props.updateGrind} />
+                    <Deliveries grind={this.props.grind} deliveries={this.props.deliveries} updateDeliveries={this.props.updateDeliveries} />
+                    <OrderSummary preferences={this.props.preferences} bean={this.props.bean} quantity={this.props.quantity} grind={this.props.grind} deliveries={this.props.deliveries} />
+                    <CreatePlanButton preferences={this.props.preferences} bean={this.props.bean} quantity={this.props.quantity} grind={this.props.grind} deliveries={this.props.deliveries} openModal={this.props.openModal} />
                 </div>
             </div>
         )
